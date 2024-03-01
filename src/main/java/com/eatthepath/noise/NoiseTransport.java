df4f3f6def08e22aa4995de7a64a4701612d60bb
Jon Chambers <jon.chambers@gmail.com>
2024-02-29 21:42:07 -0500

    Document `NoiseTransport`, `NoiseTransportReader`, and `NoiseTransportWriter`

--- src/main/java/com/eatthepath/noise/NoiseTransport.java
+++ src/main/java/com/eatthepath/noise/NoiseTransport.java
@@ -1,4 +1,11 @@
 package com.eatthepath.noise;
 
+/**
+ * A Noise transport is a bidirectional reader and writer of Noise transport messages. In the terminology of the Noise
+ * Protocol Framework specification, a {@code NoiseTransport} instance encapsulates the two "cipher states" produced by
+ * "splitting" a {@link NoiseHandshake}.
+ *
+ * @see NoiseHandshake#toTransport()
+ */
 public interface NoiseTransport extends NoiseTransportReader, NoiseTransportWriter {
 }
